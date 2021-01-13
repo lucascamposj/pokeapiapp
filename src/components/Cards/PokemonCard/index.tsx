753d432659bef6d960ff9c4f65319dec1acfe926
lucascamposj <lucascj29@gmail.com>
2021-01-13 14:14:02 -0300

    adds pokemonCard component

---
+++ src/components/Cards/PokemonCard/index.tsx
@@ -0,0 +1,125 @@
+import React, { useMemo } from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import { ActivityIndicator } from 'react-native';
+import TypesObject from '../../Sections/TypesSection/Type/types';
+
+import BackIcon from '../../../assets/svg/icons/angle-left.svg';
+import { Pokemon } from '../../../pages/Pokemon';
+import Ruler from '../../../assets/svg/icons/ruler.svg';
+import Weight from '../../../assets/svg/icons/weight.svg';
+import TypesSection from '../../Sections/TypesSection';
+import StatsSection from '../../Sections/StatsSection';
+import AbilitiesSection from '../../Sections/AbilitiesSection';
+import FormsSection from '../../Sections/FormsSection';
+
+import {
+  Container,
+  MainCard,
+  XPContainer,
+  XPText,
+  SpritesContainer,
+  Sprite,
+  SpriteSelector,
+  NameContainer,
+  NameText,
+  IdText,
+  HeightAndWeightContainer,
+  HeightContainer,
+  HeightText,
+  WeightContainer,
+  WeightText,
+  SpeciesContainer,
+  SpeciesText,
+  Separator,
+  SectionTitle,
+  Header,
+  HeaderBar,
+  BackButton,
+} from './styles';
+
+interface PokemonCardProps {
+  pokemon: Pokemon | undefined;
+  loading: boolean;
+  backButtonCallback: () => void;
+}
+
+const getPokemonFirstType = (pokemon: Pokemon | undefined) => {
+  const firstType = pokemon && pokemon?.types ? pokemon.types[0] : null;
+  const mainColor =
+    firstType && firstType?.type.name
+      ? TypesObject[firstType?.type.name].color
+      : 'red';
+  return mainColor;
+};
+
+const PokemonCard: React.FC<PokemonCardProps> = ({
+  pokemon,
+  loading,
+  backButtonCallback,
+}: PokemonCardProps) => {
+  const mainColor = useMemo(() => getPokemonFirstType(pokemon), [pokemon]);
+
+  return (
+    <>
+      {loading ? (
+        <ActivityIndicator size="large" style={{ flex: 1 }} color="red" />
+      ) : (
+        <>
+          <Header color={loading ? '#FFF' : mainColor}>
+            <HeaderBar>
+              <BackButton onPress={backButtonCallback}>
+                <BackIcon height={30} width={30} color="#FFF" />
+              </BackButton>
+            </HeaderBar>
+            <SpritesContainer>
+              <Sprite
+                resizeMode="contain"
+                source={{ uri: pokemon?.sprites.front_default }}
+              />
+              <SpriteSelector />
+            </SpritesContainer>
+          </Header>
+
+          <ScrollView scrollIndicatorInsets={{ right: 1 }}>
+            <Container>
+              <MainCard>
+                <XPContainer>
+                  <XPText>{`${pokemon?.base_experience} XP`}</XPText>
+                </XPContainer>
+                <NameContainer>
+                  <NameText>{pokemon?.name}</NameText>
+                  <IdText>{`#${pokemon?.id}`}</IdText>
+                </NameContainer>
+                <HeightAndWeightContainer>
+                  <HeightContainer>
+                    <Ruler height={28} width={28} color="#333" />
+                    <HeightText>{pokemon?.height}</HeightText>
+                  </HeightContainer>
+                  <WeightContainer>
+                    <Weight height={23} width={23} color="#333" />
+                    <WeightText>{pokemon?.weight}</WeightText>
+                  </WeightContainer>
+                </HeightAndWeightContainer>
+                <SpeciesContainer>
+                  <SpeciesText>{`species: ${pokemon?.species.name}`}</SpeciesText>
+                </SpeciesContainer>
+                <TypesSection types={pokemon?.types} />
+                <Separator />
+                <SectionTitle>Stats</SectionTitle>
+                <StatsSection stats={pokemon?.stats} />
+                <Separator />
+                <SectionTitle>Abilities</SectionTitle>
+                <AbilitiesSection abilities={pokemon?.abilities} />
+                <Separator />
+                <SectionTitle>Forms</SectionTitle>
+                <FormsSection forms={pokemon?.forms} />
+              </MainCard>
+            </Container>
+          </ScrollView>
+        </>
+      )}
+    </>
+  );
+};
+
+export default PokemonCard;
---
+++ src/components/Cards/PokemonCard/styles.ts
@@ -0,0 +1,136 @@
+import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+  width: 100%;
+  align-content: center;
+  padding: 32px 24px 16px;
+`;
+export const MainCard = styled.View`
+  width: 100%;
+`;
+export const XPContainer = styled.View``;
+export const XPText = styled.Text`
+  font-size: 25px;
+  font-family: 'RobotoMono-Light';
+`;
+export const SpritesContainer = styled.View`
+  align-items: center;
+  width: 100%;
+  height: 200px;
+  margin: 0 auto;
+  margin-top: -40px;
+  z-index: 998;
+  elevation: 18;
+`;
+
+export const Sprite = styled.Image`
+  height: 100%;
+  width: 100%;
+`;
+export const SpriteSelector = styled.View``;
+export const NameContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+export const NameText = styled.Text`
+  margin-right: 5px;
+  font-size: 30px;
+  text-transform: capitalize;
+  font-family: 'RobotoMono-Bold';
+`;
+export const IdText = styled.Text`
+  font-size: 15px;
+  color: #999;
+`;
+export const HeightAndWeightContainer = styled.View`
+  padding: 20px 20px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+export const HeightContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+export const HeightText = styled.Text`
+  font-size: 20px;
+  margin-left: 10px;
+  font-family: 'RobotoMono-Light';
+`;
+
+export const WeightContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+export const WeightText = styled.Text`
+  font-size: 20px;
+  margin-left: 10px;
+  font-family: 'RobotoMono-Light';
+`;
+export const SpeciesContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+export const SpeciesText = styled.Text``;
+export const TypesContainer = styled.View``;
+export const Type = styled.View``;
+export const Stats = styled.View``;
+export const Separator = styled.View`
+  background-color: #bbb;
+  height: 2px;
+  width: 100%;
+  margin: 10px 0;
+`;
+export const SectionTitle = styled.Text`
+  text-align: center;
+  font-family: 'RobotoMono-Bold';
+  font-size: 18px;
+`;
+export const Abilities = styled.View``;
+export const Forms = styled.View``;
+
+interface HeaderProps {
+  color: string;
+}
+export const Header = styled.View<HeaderProps>`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
+  background-color: ${({ color }) => color || '#FFF'};
+  height: 170px;
+  z-index: 998;
+  align-items: flex-start;
+  shadow-color: ${({ color }) => color || '#FFF'};
+  shadow-offset: 0 10px;
+  shadow-opacity: 0.9;
+  shadow-radius: 18px;
+
+  elevation: 17;
+`;
+
+export const HeaderBar = styled.View`
+  width: 100%;
+  z-index: 999;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  font-weight: bold;
+  font-family: 'RobotoMono-Regular';
+  line-height: 28px;
+  text-transform: capitalize;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-right: 15px;
+  z-index: 999;
+
+  elevation: 20;
+`;
